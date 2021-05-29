17ee2b3ed60278af0e495db7356fc74fcdc1c1be
Mamoru <jccjz80326@yahoo.co.jp>
2021-05-29 10:51:00 +0900

    Add GitHub Actions config yaml and Python script

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+from datetime import datetime
+
+if __name__ == '__main__':
+    doc_dir = 'doc/'
+
+    if not os.path.exists(doc_dir):
+        os.mkdir(doc_dir)
+
+    now = datetime.now()
+    html_source = now.strftime('%Y/%m/%d %H:%M:%S')
+
+    with open(doc_dir+'index.html', 'w', encoding='utf-8') as f:
+        f.write(html_source)
